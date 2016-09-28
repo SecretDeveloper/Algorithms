4d2357f172f62db4e590d900ad8f751d8712526e
Gary Kenneally <garykenneally@gmail.com>
2016-09-28 23:01:43 +0100

    first draft

---
+++ Genetic/TripPlanner/src/TripPlanner/Program.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TripPlanner
+{
+    public class Program
+    {
+        static Random _random = new Random();
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var destinations = @"Innsbruck, Austria
+Munich, Germany
+Pag, Croatia
+Venice, Italy
+Tuscany, Italy
+Florence, Italy
+Rome, Italy
+Vatican City
+Pompeii, Italy
+Gozo, Malta
+Dubrovnik, Croatia
+Santorini, Thira, Greece
+Vienna, Austria
+Prague, Czech Republic
+Krakow, Poland
+Berlin, Germany
+Amsterdam, Netherlands
+Keukenhof, Stationsweg, Lisse, Netherlands
+Glasgow, United Kingdom
+Edinburgh, United Kingdom
+Inverness, United Kingdom
+Stonehenge, Amesbury, United Kingdom
+London, United Kingdom
+Brussels, Belgium
+Paris, France
+Pamplona, Spain
+Lagos, Portugal
+Granada, Spain
+Barcelona, Spain
+Luberone, Bonnieux, France
+Nice, France
+Monte Carlo, Monaco
+Interlaken, Switzerland".Replace("\r", "").Split('\n').ToList();
+                
+                //Grab API key
+                var apikey = "INSERTAPIKEY";
+                if (File.Exists("apikey.ignore"))
+                    apikey = File.ReadAllText("apikey.ignore");
+
+#if DEBUG
+                Debugger.Launch();
+#endif
+
+                // Load destinations from file
+                if (File.Exists("destinations.txt"))
+                    destinations = File.ReadAllText("destinations.txt").Replace("\r","").Split('\n').ToList();
+
+                // Generate distance matrix if not already present 
+                // TODO: do a date check or something to see if regeneration is needed.
+                var path_distances = "distances.txt";
+                if(File.Exists(path_distances) == false)
+                    SaveDistancesToFile(destinations, path_distances, apikey);
+
+                var distanceMatrix = GenerateDistanceDictionary(ReadAsCSV(path_distances).Skip(1).ToList());
+
+                var shortest = RunDarwinian(destinations, distanceMatrix);
+                Console.Write("Shortest path found:");
+                Console.WriteLine(shortest.ToString());
+                
+#if DEBUG
+                Console.WriteLine("Enter to exit.");
+                Console.ReadLine();
+#endif
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadLine();
+            }
+        }
+
+        private static Route RunDarwinian(List<string> destinations, SortedDictionary<string, int> distanceMatrix, int generations = 1000, int populationSize = 1000, int survivorSizePerc = 10)
+        {
+            Route shortestRoute = null;
+            int nonImprovedCount = 0;
+
+            var routes = new List<Route>();
+            for (int x = 0; x < populationSize; x++) routes.Add(GenerateShuffledRoute(destinations, distanceMatrix));
+
+            for (int generation = 0; generation < generations; generation++)
+            {
+                var shortestInThisPopulation = routes.OrderBy(x=>x.Distance).First();
+                if (shortestRoute == null) shortestRoute = shortestInThisPopulation;
+                if (shortestInThisPopulation.Distance < shortestRoute.Distance)
+                {
+                    shortestRoute = shortestInThisPopulation;
+                    nonImprovedCount = 0;
+                    if (nonImprovedCount == 3) break; // have not improved in 3 generations so exit.
+                }
+                Console.WriteLine($"Generation {generation} evolved a route {shortestInThisPopulation.Distance} long.  The shortest so far is {shortestRoute.Distance}");
+
+                // grab the shortest survivors.
+                var shortestSurvivors = routes.OrderBy(x=>x.Distance).Take(populationSize/survivorSizePerc).ToList();
+
+                // rebuild population
+                var tmpRoutes = new List<Route>();
+                tmpRoutes.AddRange(shortestSurvivors);  // add shortest items
+                for (int x = 0; x < populationSize-11; x++) tmpRoutes.Add(GenerateShuffledRoute(destinations, distanceMatrix));
+                tmpRoutes.AddRange(shortestSurvivors.Select(x => MutateRoute(x, 10)));
+                tmpRoutes.AddRange(routes.Select(x => MutateRoute(x, 20)).Take(populationSize - tmpRoutes.Count)); // mutate the remaining spaces a lot
+                routes.Clear();
+                routes.AddRange(tmpRoutes);
+            }
+
+            Console.WriteLine($"All generations have executed.  The shortest path found is {shortestRoute.Distance}.");
+            return shortestRoute;
+        }
+
+        private static SortedDictionary<string, int> GenerateDistanceDictionary(IEnumerable<string[]> distanceList)
+        {
+            var dict = new SortedDictionary<string, int>();
+            foreach (var distancePair in distanceList)
+            {
+                dict.Add(Tokenize(distancePair[0], distancePair[1]), int.Parse(distancePair[2]));
+            }
+            return dict;
+
+            //return distanceList.ToDictionary(distanceItem => Tokenize(distanceItem[0], distanceItem[1]), distanceItem => int.Parse(distanceItem[2]));
+        }
+
+       private static SortedDictionary<int, List<string>> CalculatePopulationDistances(List<List<string>> populations, SortedDictionary<string, int> distanceList)
+        {
+            var scores = new SortedDictionary<int, List<string>>();
+            foreach (var population in populations)
+            {
+                var currentLocation = "";
+                var distanceTotal = 0;
+                foreach (var destination in population)
+                {
+                    if (currentLocation == "")
+                    {
+                        currentLocation = destination;
+                        continue;
+                    }
+                    distanceTotal += distanceList[Tokenize(currentLocation, destination)];
+                }
+                scores.Add(distanceTotal, population);
+            }
+            return scores;
+        }
+
+        private static Route MutateRoute(Route route, int mutations)
+        {
+            var tmp = new Leg();
+            
+            for (int mut = 0; mut < mutations; mut++)
+            {
+                var x = _random.Next(0, route.Legs.Count - 1);
+                var y = _random.Next(0, route.Legs.Count - 1);
+                tmp = route.Legs[x];
+                route.Legs[x] = route.Legs[y];
+                route.Legs[y] = tmp;
+            }
+            return route;
+        }
+
+        private static Route GenerateShuffledRoute(List<string> destinations, SortedDictionary<string, int> distanceMatrix)
+        {
+            List<string> shuffled = Shuffle(Clone(destinations));
+            return new Route(shuffled, distanceMatrix);
+        }
+
+        public static string Tokenize(string s, string s1)
+        {
+            return (s + s1).Replace(" ", "_").Replace("\r", "");
+        }
+
+        private static List<string> Clone(List<string> source)
+        {
+            List<string> tmp = new List<string>();
+            foreach (var destination in source)
+            {
+                tmp.Add(destination);
+            }
+            return tmp;
+        }
+
+        public static List<string> Shuffle(List<string> source)
+        {
+            int n = source.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = _random.Next(n + 1);
+                string value = source[k];
+                source[k] = source[n];
+                source[n] = value;
+            }
+            return source;
+        }
+
+
+        //even cooler as an extension method
+        static IEnumerable<string[]> ReadAsCSV(string filename, char seperator = '\t')
+        {
+            using (var reader = new StreamReader(new FileStream(filename, FileMode.Open)))
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var data = line.Split('\t');
+                    yield return data;
+                }
+        }
+
+        /// <summary>
+        /// Get a list of distances between all points in the provided destinations file.
+        /// N^2-N calls will be made.
+        /// </summary>
+        /// <param name="destinations"></param>
+        /// <param name="pathDistances"></param>
+        private static void SaveDistancesToFile(List<string> destinations, string pathDistances, string apikey)
+        {
+            Console.WriteLine("Gathering distances.");
+
+            using (var writer = new StreamWriter(new FileStream(pathDistances, FileMode.Create))) 
+            {
+                string header = "destinationA\tdestinationB\tdistance";
+                writer.Write(header);
+
+                foreach (var destinationA in destinations)
+                {
+                    foreach (var destinationB in destinations)
+                    {
+                        if (destinationA.Equals(destinationB)) continue;
+                        try
+                        {
+                            var distance = GetDistanceFromGoogle(destinationA, destinationB, apikey);
+                            writer.Write("\n" + destinationA + "\t" + destinationB +"\t"+distance);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error when getting distances for {0} and {1}", destinationA, destinationB);
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static int GetDistanceFromGoogle(string destinationA, string destinationB, string apikey)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("http://maps.googleapis.com/maps/api/distancematrix/json?origins="
+                                               + destinationA + "&destinations=" + destinationB
+                                               + "&apikey=" + apikey
+                                               + "&mode=driving&language=us-en&sensor=false&units=metric");
+
+                var responseString = response.Result.Content.ReadAsStringAsync().Result;
+                dynamic data = JsonConvert.DeserializeObject(responseString);
+                return data.rows[0].elements[0].distance.value;
+            }
+
+        }
+    }
+
+    internal class Route
+    {
+        public static string Tokenize(string s, string s1)
+        {
+            return (s + s1).Replace(" ", "_").Replace("\r", "");
+        }
+
+        public List<Leg> Legs { get; set; }
+
+        public Route()
+        {
+            Legs = new List<Leg>();
+        }
+
+        public Route(List<string> destinations, SortedDictionary<string, int> distanceMatrix)
+        {
+            Legs = new List<Leg>();
+            string currentLocation = "";
+            foreach (var destination in destinations)
+            {
+                if (currentLocation == "")
+                {
+                    currentLocation = destination;
+                    continue;
+                }
+                var leg = new Leg();
+                leg.Start = currentLocation;
+                leg.Finish = destination;
+                leg.Distance = distanceMatrix[Tokenize(currentLocation, destination)];
+                Legs.Add(leg);
+                currentLocation = destination;
+            }
+        }
+
+        public int Distance
+        {
+            get
+            {
+                return Legs.Sum(x => x.Distance);
+            } 
+        }
+
+        public override string ToString()
+        {
+            string st = $"Route is {Distance/1000}km long";
+            Legs.ForEach(x=> st = st + "\n"+x.ToString());
+
+            return st;
+        }
+    }
+
+    internal class Leg
+    {
+        public string Start { get; set; }
+        public string Finish { get; set; }
+        public int Distance { get; set; }
+
+        public override string ToString()
+        {
+            return this.Start + " --> " + this.Finish + " - " + this.Distance;
+        }
+    }
+}
---
+++ Genetic/TripPlanner/src/TripPlanner/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TripPlanner")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("959a8616-d05a-44eb-9c2f-209982129ffb")]
